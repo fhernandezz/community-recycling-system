f2ad4f3ea22382be3f2606aa30d345cf95c625a6
Fabricio30HL <fabriciohernandez3026@gmail.com>
2026-05-16 22:14:32 -0600

    cambios al frame de main app

--- src/ucr/ac/cr/views/main_app.py
+++ src/ucr/ac/cr/views/main_app.py
@@ -1,19 +1,33 @@
 import tkinter as tk
 from tkinter import ttk
 
-from .recycler_view import RecyclerView
-from .collection_point_view import CollectionPointView
-from .record_view import RecordView
+from src.ucr.ac.cr.views.recycler_view import RecyclerView
+from src.ucr.ac.cr.views.collection_point_view import CollectionPointView
+from src.ucr.ac.cr.views.record_view import RecordView
+from src.ucr.ac.cr.views.reports_view import ReportsView
 
 class MainApp(tk.Tk):
     def __init__(self, recycler_controller, point_controller, record_controller):
         super().__init__()
         self.title("Sistema de Reciclaje Comunitario")
-        self.geometry("900x600")
+        self.geometry("1000x650")
 
         notebook = ttk.Notebook(self)
         notebook.pack(fill="both", expand=True)
 
-        notebook.add(RecyclerView(notebook, recycler_controller), text="Recicladores")
-        notebook.add(CollectionPointView(notebook, point_controller), text="Puntos")
-        notebook.add(RecordView(notebook, record_controller), text="Registros")
+        notebook.add(
+            RecyclerView(notebook, recycler_controller),
+            text="Recicladores"
+        )
+        notebook.add(
+            CollectionPointView(notebook, point_controller),
+            text="Puntos de Recolección"
+        )
+        notebook.add(
+            RecordView(notebook, recycler_controller, point_controller, record_controller),
+            text="Entregas"
+        )
+        notebook.add(
+            ReportsView(notebook, record_controller),
+            text="Reportes"
+        )
